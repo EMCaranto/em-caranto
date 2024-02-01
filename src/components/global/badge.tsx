93f89d7635ccb4b297128adc3a80047426bf49a3
Erickson Caranto <erickson.caranto.m@gmail.com>
2024-02-01 18:26:14 +0800

    :hammer: Add badges on hero component

--- src/components/global/badge.tsx
+++ src/components/global/badge.tsx
@@ -1,8 +1,40 @@
+'use client'
+
 // React JS
 import React from 'react'
 
-const Badge = () => {
-  return <div>Badge</div>
+// Dependencies
+import CountUp from 'react-countup'
+
+interface BadgeProps {
+  icon?: React.ReactNode
+  containerStyle?: string
+  badgeText?: string
+  endCountText?: string
+  endCountNum: number
+}
+
+const Badge = ({
+  icon,
+  containerStyle,
+  badgeText,
+  endCountText,
+  endCountNum,
+}: BadgeProps) => {
+  return (
+    <div className={`custom-badge ${containerStyle}`}>
+      <div className="text-3xl text-primary">{icon}</div>
+      <div className="flex items-center gap-x-2">
+        <div className="text-4xl font-bold leading-none">
+          <CountUp end={endCountNum} delay={1} duration={4} />
+          {endCountText}
+        </div>
+        <div className="max-w-[70px] text-sm font-medium leading-none text-black">
+          {badgeText}
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Badge
--- src/components/global/hero.tsx
+++ src/components/global/hero.tsx
@@ -17,6 +17,7 @@ import {
 import Badge from '@/components/global/badge'
 import ImageDev from '@/components/global/image-dev'
 import Social from '@/components/global/social'
+
 import { Button } from '@/components/ui/button'
 
 // Public
@@ -24,7 +25,7 @@ import myImage from '../../../public/assets/hero/developer.png'
 
 const Hero = () => {
   return (
-    <section className="bg-hero h-[80vh] bg-cover bg-bottom bg-no-repeat py-12 dark:bg-none xl:py-24 xl:pt-28">
+    <section className="h-[80vh] bg-hero bg-cover bg-bottom bg-no-repeat py-12 dark:bg-none xl:py-24 xl:pt-28">
       <div className="container mx-auto">
         <div className="flex justify-between gap-x-8">
           {/** text */}
@@ -62,9 +63,30 @@ const Hero = () => {
           </div>
           {/** image */}
           <div className="relative hidden xl:flex">
-            {/** badge */}
-            <Badge />
-            <div className="bg-hero-shape-light dark:bg-hero-shape-dark absolute -right-2 -top-1 h-[500px] w-[500px] bg-no-repeat">
+            {/** badge 1 */}
+            <Badge
+              containerStyle={'absolute -left-[5rem] top-[24%]'}
+              icon={<RiBriefcase4Fill />}
+              endCountNum={3}
+              badgeText={'Years of Experience'}
+            />
+            {/** badge 2 */}
+            <Badge
+              containerStyle={'absolute -left-[1rem] top-[80%]'}
+              icon={<RiTodoFill />}
+              endCountNum={6}
+              endCountText={'k'}
+              badgeText={'Finished Projects'}
+            />
+            {/** badge 3 */}
+            <Badge
+              containerStyle={'absolute -right-8 top-[55%]'}
+              icon={<RiTeamFill />}
+              endCountNum={9}
+              endCountText={'k'}
+              badgeText={'Happy Clients'}
+            />
+            <div className="absolute -right-2 -top-1 h-[500px] w-[500px] bg-hero-shape-light bg-no-repeat dark:bg-hero-shape-dark">
               {/** blob */}
             </div>
             <ImageDev
