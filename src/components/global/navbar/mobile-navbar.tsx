8ee61867613ff9ec4fc3bd133d4ca0fffba2f367
Erickson Caranto <erickson.caranto.m@gmail.com>
2024-02-01 18:26:48 +0800

    :hammer: Add links on mobile navbar component

--- src/components/global/navbar/mobile-navbar.tsx
+++ src/components/global/navbar/mobile-navbar.tsx
@@ -1,8 +1,37 @@
 // React JS
 import React from 'react'
 
+// Dependencies
+import { MenuIcon } from 'lucide-react'
+
+// Components
+import MainNavbar from '@/components/global/navbar/main-navbar'
+
+import Logo from '@/components/global/logo'
+import Social from '@/components/global/social'
+
+import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet'
+
 const MobileNavbar = () => {
-  return <div>MobileNavbar</div>
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <MenuIcon className="h-4 w-4 cursor-pointer" />
+      </SheetTrigger>
+      <SheetContent>
+        <div className="flex h-full flex-col items-center justify-between py-8">
+          <div className="flex flex-col items-center gap-y-32">
+            <Logo />
+            <MainNavbar
+              containerStyle={'flex flex-col items-center gap-y-6'}
+              linkStyle={'text-2xl'}
+            />
+          </div>
+          <Social containerStyle={'flex gap-x-4'} iconStyle={'text-2xl'} />
+        </div>
+      </SheetContent>
+    </Sheet>
+  )
 }
 
 export default MobileNavbar
