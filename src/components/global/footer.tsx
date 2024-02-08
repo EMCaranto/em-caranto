06933a97696c6f726cf22125eb5570d4075b25bd
Erickson Caranto <erickson.caranto.m@gmail.com>
2024-02-08 16:15:27 +0800

    :hammer: Add social links on footer on home page

--- src/components/global/footer.tsx
+++ src/components/global/footer.tsx
@@ -1,8 +1,29 @@
 // React JS
 import React from 'react'
 
+// Components
+import Social from '@/components/global/social'
+
 const Footer = () => {
-  return <div>Footer</div>
+  return (
+    <footer className="bg-secondary py-12">
+      <div className="container mx-auto">
+        <div className="flex flex-col items-center justify-between">
+          {/** social */}
+          <Social
+            containerStyle={'mx-auto mb-4 flex gap-x-6 xl:mx-0'}
+            iconStyle={
+              'text-[20px] text-primary transition-all hover:text-primary/70 dark:text-white/70 dark:hover:text-primary'
+            }
+          />
+          {/** copyright */}
+          <div className="text-muted-foreground">
+            Copyright &copy; Erickson Caranto. All right reserved.
+          </div>
+        </div>
+      </div>
+    </footer>
+  )
 }
 
 export default Footer
